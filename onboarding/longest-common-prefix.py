950a8e286ab00cc84f4a81bf6b645e8f7e1862f8
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2023-11-29 18:10:57 +0300

    Added solution for Longest Common Prefix

---
+++ onboarding/longest-common-prefix.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        max = strs[0]
+
+        for i in range(1,len(strs)):
+            if len(strs[i]) < len(max):
+                max = max[:len(strs[i])]
+            for j in range(len(strs[i])):
+                if j < len(max) and strs[i][j] != max[j]:
+                    max = max[:j]
+                    break
+        return max
+            
+        
