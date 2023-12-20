1f1e792edfd6dd92fa4cc523e06e16d8faf522ed
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2023-12-20 18:59:22 +0300

    Added solution for Transpose Matrix

---
+++ onboarding/transpose-matrix.py
@@ -0,0 +1,9 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        n = len(matrix)
+        m = len(matrix[0])
+        ans = [[0 for i in range(n)] for i in range(m)]
+        for i in range(n):
+            for j in range(m):
+                ans[j][i] = matrix[i][j]
+        return ans
