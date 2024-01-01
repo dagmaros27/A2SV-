9f21cc8fb361267897cbda91fb0689916d1a79db
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-01-01 19:37:13 +0300

    Added solution for Maximum Ice Cream Bars

---
+++ progress-sheet/maximum-ice-cream-bars.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        
+        maxNum = max(costs)
+        counter = [0]* (maxNum+1)
+
+        for cost in costs:
+            counter[cost] += 1
+        print(maxNum)
+        ans = 0
+        total= 0
+        for i in range(maxNum+1):
+            while counter[i] > 0:
+                total += i 
+                if total <= coins:
+                    ans += 1
+                counter[i] -= 1
+                
+        
+        return ans
