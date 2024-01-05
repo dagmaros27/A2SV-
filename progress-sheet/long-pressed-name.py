0adb2bb71739d66b56c2459e86184dc95a218cd2
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-01-05 22:34:11 +0300

    Added solution for Long Pressed Name

---
+++ progress-sheet/long-pressed-name.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+
+        i, j = 0, 0
+
+        if len(name) > len(typed):
+            return False
+        
+        while i < len(name) and j < len(typed):
+            if name[i] == typed[j]:
+                j += 1
+                i += 1
+            elif j > 0 and typed[j] == typed[j-1]:
+                j += 1
+            else:
+                return False
+        
+        while j < len(typed) and typed[j] == typed[j-1]:
+            j += 1
+
+        return i == len(name) and j == len(typed)
