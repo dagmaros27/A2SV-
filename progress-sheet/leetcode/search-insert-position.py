a76a18dd77a1cc54d195c25c32e572a88ff9135d
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-03-13 22:51:39 +0300

    Add solution for Search Insert Position

---
+++ progress-sheet/leetcode/search-insert-position.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l,h = 0, len(nums)-1
+
+        while l <= h:
+            m = (l+h)//2
+            if nums[m] > target:
+                h = m-1
+            elif nums[m] < target:
+                l = m+1
+            else:
+                return m
+        return l
+        
